0324d575cd136085f5ab22f012b8caf125fea4cf
saly0781 <142720434+saly0781@users.noreply.github.com>
2025-08-11 17:03:31 +0200

    v1.3.12

--- jScript.js
+++ jScript.js
@@ -1468,11 +1468,19 @@ async function initializeApp(optionData) {
                 list = list.slice(0, 15);
             }
             localStorage.setItem(CONTINUE_WATCHING_KEY, JSON.stringify(list));
+            window.flutter_inappwebview.callHandler('playerAction', {
+                action: 'saveEpisodeProgress',
+                data: list
+            });
         };
         const removeEpisodeProgress = (movieId) => {
             let list = getContinueWatchingList();
             const updatedList = list.filter(item => item.movieId !== movieId);
             localStorage.setItem(CONTINUE_WATCHING_KEY, JSON.stringify(updatedList));
+            window.flutter_inappwebview.callHandler('playerAction', {
+                action: 'saveEpisodeProgress',
+                data: list
+            });
         };
         const getSavedEpisode = (movieId) => {
             const list = getContinueWatchingList();
@@ -2234,10 +2242,7 @@ async function initializeApp(optionData) {
                 const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent);
                 if (isIOS && art.fullscreen && window.device == "web") {
                     // console.log("Exiting fullscreen for ad playback on iOS");
-                    window.device == "web" ? art.fullscreen = false : window.flutter_inappwebview.callHandler('playerAction', {
-                        action: 'fullscreen',
-                        data: "exit"
-                    });
+                    window.device == "web" ? art.fullscreen = false : "";
                 }
             };
 
